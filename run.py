7d319e921e36e6d5d793cd213a314c300db8dbc3
kasemdeautsch <xdow69x0@students.codeinstitute.net>
2025-07-26 11:29:06 +0200

    done

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
+if os.path.exists("env.py"):
+    import env  # noqa: E402
+
+# Initialize the Flask application
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -27,18 +32,23 @@ def about_member(member_url):
         info = json.load(json_data)
         # print("info...", info)
         # print("info_type.", type(info))
-        
+
         for obj in info:
             if obj["url"] == member_url:
                 member = obj
                 print("member...", member)
     return render_template('member.html', member=member)
-    
 
-
-
-@app.route("/contactus")
+# Contact Us route
+@app.route("/contactus", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        print("request...", request)
+        print("request.form...", request.form)
+        print("Name...", request.form["email"])
+        
+        flash("Thanks {}, we have received your message".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -48,6 +58,9 @@ def work():
 
 
 print("name...", __name__)
+
+# Check if the script is run directly
+# and not imported as a module
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
